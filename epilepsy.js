a0a419fa2482af87416afb45d6c603384c90d21e
JuanTheDeagle <78816028+JuanTheDeagle@users.noreply.github.com>
2021-07-09 12:32:57 -0400

    Add files via upload

---
+++ epilepsy.js
@@ -0,0 +1,22 @@
+
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+  background(30);
+  if (keyDown('up')){
+    background('red')}
+  if (keyDown('down')){
+    background('yellow')}
+  if (keyDown('right')){
+    background('green')}
+  if (keyDown('left')){
+    background('blue')}
+}
+
+
+
+
+
